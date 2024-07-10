b2f7102b0dc3aac008aa80cb5f94878bd74843de
Hugo <381093478@qq.com>
2024-07-10 14:17:31 +0800

    Finishing touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,55 @@
 import * as THREE from 'three'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { Canvas, extend, useThree, useFrame } from '@react-three/fiber'
+import { useGLTF, useTexture, Environment, Lightformer, PerspectiveCamera, Center, RenderTexture, Text3D } from '@react-three/drei'
 import { BallCollider, CuboidCollider, Physics, RigidBody, useRopeJoint, useSphericalJoint } from '@react-three/rapier'
 import { MeshLineGeometry, MeshLineMaterial } from 'meshline'
+import { useControls } from 'leva'
 
 extend({ MeshLineGeometry, MeshLineMaterial })
 
 const App = () => {
+  const { debug } = useControls({ debug: false })
   return (
     <Canvas camera={{ position: [0, 0, 13], fov: 25 }}>
-      <Physics debug interpolate gravity={[0, -40, 0]} timeStep={1 / 60}>
+      <ambientLight intensity={Math.PI} />
+      <Physics debug={debug} interpolate gravity={[0, -40, 0]} timeStep={1 / 60}>
         <Band />
       </Physics>
+      <Environment background blur={0.75}>
+        <color attach="background" args={['black']} />
+        <Lightformer intensity={2} color="white" position={[0, -1, 5]} rotation={[0, 0, Math.PI / 3]} scale={[100, 0.1, 1]} />
+        <Lightformer intensity={3} color="white" position={[-1, -1, 1]} rotation={[0, 0, Math.PI / 3]} scale={[100, 0.1, 1]} />
+        <Lightformer intensity={3} color="white" position={[1, 1, 1]} rotation={[0, 0, Math.PI / 3]} scale={[100, 0.1, 1]} />
+        <Lightformer intensity={10} color="white" position={[-10, 0, 14]} rotation={[0, Math.PI / 2, Math.PI / 3]} scale={[100, 10, 1]} />
+      </Environment>
     </Canvas>
   )
 }
 
-function Band() {
+function Band({ maxSpeed = 50, minSpeed = 10 }) {
   const band = useRef(), fixed = useRef(), j1 = useRef(), j2 = useRef(), j3 = useRef(), card = useRef() // prettier-ignore
   const vec = new THREE.Vector3(), ang = new THREE.Vector3(), rot = new THREE.Vector3(), dir = new THREE.Vector3() // prettier-ignore
+  const segmentProps = { type: 'dynamic', canSleep: true, colliders: false, angularDamping: 2, linearDamping: 2 }
+  const { nodes, materials } = useGLTF('badge.glb')
+  const badgeTexture = useTexture('cam-badge-texture.png')
+  const bandTexture = useTexture('cam-band-texture.png')
   const { width, height } = useThree((state) => state.size)
   const [curve] = useState(() => new THREE.CatmullRomCurve3([new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3(), new THREE.Vector3()]))
   const [dragged, drag] = useState(false)
+  const [hovered, hover] = useState(false)
 
   useRopeJoint(fixed, j1, [[0, 0, 0], [0, 0, 0], 1]) // prettier-ignore
   useRopeJoint(j1, j2, [[0, 0, 0], [0, 0, 0], 1]) // prettier-ignore
   useRopeJoint(j2, j3, [[0, 0, 0], [0, 0, 0], 1]) // prettier-ignore
   useSphericalJoint(j3, card, [[0, 0, 0], [0, 1.45, 0]]) // prettier-ignore
+
+  useEffect(() => {
+    if (hovered) {
+      document.body.style.cursor = dragged ? 'grabbing' : 'grab'
+      return () => void (document.body.style.cursor = 'auto')
+    }
+  }, [hovered, dragged])
 
   useFrame((state, delta) => {
     if (dragged) {
@@ -37,10 +60,16 @@ function Band() {
       card.current?.setNextKinematicTranslation({ x: vec.x - dragged.x, y: vec.y - dragged.y, z: vec.z - dragged.z })
     }
     if (fixed.current) {
-      // Calculate catmul curve      
+      // Fix most of the jitter when over pulling the card
+      [j1, j2].forEach((ref) => {
+        if (!ref.current.lerped) ref.current.lerped = new THREE.Vector3().copy(ref.current.translation())
+        const clampedDistance = Math.max(0.1, Math.min(1, ref.current.lerped.distanceTo(ref.current.translation())))
+        ref.current.lerped.lerp(ref.current.translation(), delta * (minSpeed + clampedDistance * (maxSpeed - minSpeed)))
+      })
+      // Calculate catmul curve
       curve.points[0].copy(j3.current.translation())
-      curve.points[1].copy(j2.current.translation())
-      curve.points[2].copy(j1.current.translation())
+      curve.points[1].copy(j2.current.lerped)
+      curve.points[2].copy(j1.current.lerped)
       curve.points[3].copy(fixed.current.translation())
       band.current.geometry.setPoints(curve.getPoints(32))
       // Tilt it back towards the screen
@@ -50,32 +79,79 @@ function Band() {
     }
   })
 
+  curve.curveType = 'chordal'
+  bandTexture.wrapS = bandTexture.wrapT = THREE.RepeatWrapping
+
+  function BadgeTexture({ user }) {
+    return (
+      <>
+        <PerspectiveCamera makeDefault manual aspect={1.05} position={[0.49, 0.22, 2]} />
+        <mesh geometry={nodes.card.geometry}>
+          <planeGeometry args={[0.95, -0.95 / 0.7]} />
+          <meshBasicMaterial transparent={true} alphaMap={badgeTexture} side={THREE.BackSide} />
+        </mesh>
+        <Center position={[-0.22, 0.25, 0]}>
+            <Text3D
+              bevelEnabled={false}
+              bevelSize={0}
+              font="/Geist_Regular.json"
+              height={0}
+              size={0.07}
+              rotation={[0, Math.PI, Math.PI]}>
+              {user.firstName}
+            </Text3D>
+            <Text3D
+              bevelEnabled={false}
+              bevelSize={0}
+              font="/Geist_Regular.json"
+              height={0}
+              size={0.07}
+              position={[0, 0.09, 0]}
+              rotation={[0, Math.PI, Math.PI]}>
+              {user.lastName}
+            </Text3D>
+        </Center>
+      </>
+    )
+  }
+
   return (
     <>
       <group position={[0, 4, 0]}>
-        <RigidBody ref={fixed} angularDamping={2} linearDamping={2} type="fixed" />
-        <RigidBody position={[0.5, 0, 0]} ref={j1} angularDamping={2} linearDamping={2}>
+        <RigidBody ref={fixed} {...segmentProps} type="fixed" />
+        <RigidBody position={[0.5, 0, 0]} ref={j1} {...segmentProps}>
           <BallCollider args={[0.1]} />
         </RigidBody>
-        <RigidBody position={[1, 0, 0]} ref={j2} angularDamping={2} linearDamping={2}>
+        <RigidBody position={[1, 0, 0]} ref={j2} {...segmentProps}>
           <BallCollider args={[0.1]} />
-        </RigidBody >
-        <RigidBody position={[1.5, 0, 0]} ref={j3} angularDamping={2} linearDamping={2}>
+        </RigidBody>
+        <RigidBody position={[1.5, 0, 0]} ref={j3} {...segmentProps}>
           <BallCollider args={[0.1]} />
-        </RigidBody >
-        <RigidBody position={[2, 0, 0]} ref={card} angularDamping={2} linearDamping={2} type={dragged ? 'kinematicPosition' : 'dynamic'} >
+        </RigidBody>
+        <RigidBody position={[2, 0, 0]} ref={card} {...segmentProps} type={dragged ? 'kinematicPosition' : 'dynamic'}>
           <CuboidCollider args={[0.8, 1.125, 0.01]} />
-          <mesh
+          <group
+            scale={2.25}
+            position={[0, -1.2, -0.05]}
+            onPointerOver={() => hover(true)}
+            onPointerOut={() => hover(false)}
             onPointerUp={(e) => (e.target.releasePointerCapture(e.pointerId), drag(false))}
             onPointerDown={(e) => (e.target.setPointerCapture(e.pointerId), drag(new THREE.Vector3().copy(e.point).sub(vec.copy(card.current.translation()))))}>
-            <planeGeometry args={[0.8 * 2, 1.125 * 2]} />
-            <meshBasicMaterial transparent opacity={0.25} color="white" side={THREE.DoubleSide} />
-          </mesh>
-        </RigidBody >
-      </group >
+            <mesh geometry={nodes.card.geometry}>
+              <meshPhysicalMaterial iridescence={1} iridescenceIOR={1} iridescenceThicknessRange={[0, 2400]} clearcoat={1} clearcoatRoughness={0.15} roughness={0.3} metalness={0.5}>
+                <RenderTexture attach="map" height={2000} width={2000}>
+                  <BadgeTexture user={{ firstName: "Hu", lastName: "Hongwei" }} />
+                </RenderTexture>
+              </meshPhysicalMaterial>
+            </mesh>
+            <mesh geometry={nodes.clip.geometry} material={materials.metal} material-roughness={0.3} />
+            <mesh geometry={nodes.clamp.geometry} material={materials.metal} />
+          </group>
+        </RigidBody>
+      </group>
       <mesh ref={band}>
         <meshLineGeometry />
-        <meshLineMaterial transparent opacity={0.25} color="white" depthTest={false} resolution={[width, height]} lineWidth={1} />
+        <meshLineMaterial color="white" depthTest={false} resolution={[width, height]} useMap map={bandTexture} repeat={[-3, 1]} lineWidth={1} />
       </mesh>
     </>
   )
